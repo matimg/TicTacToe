3a60a3d2ea2e21b27bcfe6f40b75f5eddb92ad8f
Matías Gandini <matias.gandinimg@gmail.com>
2021-04-30 13:04:11 +0000

    menu V1

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,18 +7,34 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="p-5" style={{ backgroundColor: "#4a453f" }}>
+				<h6 className="text-white">
+					<b>CHOOSE YOUR WEAPON</b>
+				</h6>
+				<input type="text" placeholder="Player 1 username" />
+				<input type="text" placeholder="Player 2 username" />
+				<br />
+				<button
+					className="btn mr-2 mt-2 display-4"
+					style={{
+						color: "#edd97d",
+						backgroundColor: "#3e3732",
+						width: "70px",
+						height: "70px"
+					}}>
+					X
+				</button>
+				<button
+					className="btn ml-2 mt-2"
+					style={{
+						color: "#3facd6",
+						backgroundColor: "#3e3732",
+						width: "70px",
+						height: "70px"
+					}}>
+					O
+				</button>
+			</div>
 		</div>
 	);
 }
